060b78ec6d55bc0489c8549465dca7d71aed1bd0
Usman Haider <uhaider461@gmail.com>
2023-11-07 04:21:58 +0500

    - handle Games with no images

--- src/services/image-url.ts
+++ src/services/image-url.ts
@@ -1,5 +1,6 @@
+import placeholderImage from "../assets/no-image-placeholder.webp";
 const getCroppedImageUrl = (url: string): string => {
-  if (!url) return "";
+  if (!url) return placeholderImage;
   const target: string = "media/";
   const index: number = url.indexOf(target) + target.length;
   return url.slice(0, index) + "crop/600/400/" + url.slice(index);
